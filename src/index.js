612db5139f252bbc51cc7da7883df7af92856851
terotuomala <tero.tuomala@protonmail.com>
2022-07-16 19:49:13 +0300

    Update configuration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+  res.end('Hello World!')
 });
 
 const port = 3000
